f2f00c32199b57031bc6115732a329c7a7c6068a
JBrinco <j.brinco@campus.fct.unl.pt>
2024-12-17 15:12:09 +0000

    Added copyright notice to scripts

--- chromacalc.py
+++ chromacalc.py
@@ -1,5 +1,21 @@
 #!/bin/python
 
+## LICENSE STUFF ###
+# This file is part of Chromapy (https://github.com/JBrinco/Chromapy).
+# Copyright (c) 2024 João Brinco.
+# 
+# This program is free software: you can redistribute it and/or modify  
+# it under the terms of the GNU General Public License as published by  
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 import argparse
 import chromapy
 
--- chromapy/chromacalc.py
+++ chromapy/chromacalc.py
@@ -3,7 +3,7 @@
 ChromaPython - Chromatography-related Calculators
 #########################################
 
-Copyright (C) 2022-2024 João Brinco
+Copyright (C) 2024 João Brinco
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
 
